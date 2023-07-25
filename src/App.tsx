e2e05a4ffbfa561662eae40a843638f6aee58ee9
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-25 18:36:15 +0300

    feat: add store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+
 function App() {
+	const posts = useSelector((store: RootState) => store.posts.posts);
+
 	return (
-		<h1 className="text-3xl font-bold underline">react-rtk-table-search-sort-filter-pagination</h1>
+		<>
+			<h1 className="text-3xl font-bold underline">react-rtk-table-search-sort-filter-pagination</h1>
+			<h2>{posts.length}</h2>
+		</>
 	);
 }
 
