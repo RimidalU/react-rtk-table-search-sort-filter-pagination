313e50f63e07aa44284c9d9cfc34a9bf6605a4a8
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-25 11:19:15 +0300

    lib: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-	return <h1>react-rtk-table-search-sort-filter-pagination</h1>;
+	return (
+		<h1 className="text-3xl font-bold underline">react-rtk-table-search-sort-filter-pagination</h1>
+	);
 }
 
 export default App;
