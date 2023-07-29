7385f5d5c400920d19f3ccb21018e4e69fa931c0
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-30 00:28:37 +0300

    feat: add .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import { Post } from "./../types";
 
+const baseUrl = import.meta.env.VITE_APP_BASE_URL
+
 export const getPosts = async (): Promise<Post[]> => {
 	try {
-		const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+		const response = await fetch(`${baseUrl}posts`);
 		if (!response.ok) {
 			throw new Error(`HTTP Response Code: ${response.status}`);
 		}
